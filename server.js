39a3592f1da9e8763f095ef56854cff4629da478
Yashwant2704 <yashwantnagarkar04@gmail.com>
2025-06-10 11:35:31 +0530

    Add CORS

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const expenseRoutes = require('./routes/expenseRoutes');
 const settlementRoutes = require('./routes/settlementRoutes');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
