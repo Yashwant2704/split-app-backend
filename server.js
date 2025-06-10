628965e644fee251db06cd57eb9432091ae4b6cf
Yashwant2704 <yashwantnagarkar04@gmail.com>
2025-06-10 11:29:26 +0530

    Add CORS

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const settlementRoutes = require('./routes/settlementRoutes');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/expenses', expenseRoutes);
